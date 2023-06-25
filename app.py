5b0f9a6d330f1be967ca0fca06c2bd92fa5152a0
aglantzrbc <adamglantz@yahoo.com>
2023-06-25 18:26:08 -0400

    initial commit

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
+#################################################
 # TASK: Create welcome route, then list all available api routes
 # Create a welcome route
 @app.route("/")
@@ -60,6 +61,7 @@ def welcome():
         "<p><small><strong>Note:</strong> All the links assume the user employs <strong>port 5000</strong> for Flask output.<br/>If not, the links won't work, and you'll have to paste each route into your browser preceded by '<strong>http://localhost:XXXX/</strong>', where '<strong>XXXX</strong>' is your port of choice.<br/><em>Example:</em> <a href=\"/api/v1.0/2016-08-23/2017-08-23\">http://localhost:XXXX/api/v1.0/start</a>.</small></p>"
     )
 
+#################################################
 '''TASK: Convert the query results from the precipitation analysis (i.e. retrieve only the last 12 months of data) to a dictionary using [date] as the key and [prcp] as the value. Return the JSON representation of the dictionary.'''
 # Create a precipitation route
 @app.route("/api/v1.0/precipitation")
@@ -93,6 +95,7 @@ def precipitation():
     # Return a JSON list of precipitation_query_values
     return jsonify(precipitaton_query_values)
 
+#################################################
 # TASK: Return a JSON list of stations from the dataset
 # Create a station route
 @app.route("/api/v1.0/stations")
@@ -118,67 +121,43 @@ def station():
     # Return a JSON list of stations_values
     return jsonify (stations_values) 
 
+#################################################
 '''TASK: Query the dates and temperature observations of the most-active station for the previous year of data. Return a JSON list of temperature observations for the previous year.'''
 # Create a tobs route
-@app.route("/api/v1.0/tobs") 
+@app.route("/api/v1.0/tobs")
 def tobs():
-
     # Establish session
     session = Session(engine)
-    
-    """Return a list of dates and temps observed for the most active station for the last year of data from the database""" 
-    # Create query to find the last date in the database
-    
-    last_year_query_results = session.query(Measurement.date).\
-        order_by(Measurement.date.desc()).first() 
 
-    print(last_year_query_results)
-    # last_year_date returns row ('2017-08-23',), use this to create a date time object to find start query date 
-    
-    # check to see if last year was correctly returned by creating dictionary to return last year value to browser in JSON format
-    last_year_query_values = []
-    for date in last_year_query_results:
-        last_year_dict = {}
-        last_year_dict["date"] = date
-        last_year_query_values.append(last_year_dict) 
-    print(last_year_query_values)
-    # returns: [{'date': '2017-08-23'}]
+    # Retrieve the most recent date in the dataset
+    latest_date = session.query(Measurement.date).order_by(Measurement.date.desc()).first()
+    latest_date = dt.datetime.strptime(latest_date[0], "%Y-%m-%d").date()
 
-    # Create query_start_date by finding the difference between date time object of "2017-08-23" - 365 days
-    query_start_date = dt.date(2017, 8, 23)-dt.timedelta(days =365) 
-    print(query_start_date) 
-    # returns: 2016-08-23 
+    # Calculate the date one year ago from the most recent date
+    one_year_ago = latest_date - dt.timedelta(days=365)
 
-    # Create query to find most active station in the database 
+    # Query the most active station for the temperature observations within the last year
+    most_active_station = session.query(Measurement.station).\
+        group_by(Measurement.station).\
+        order_by(func.count(Measurement.station).desc()).first()
 
-    active_station= session.query(Measurement.station, func.count(Measurement.station)).\
-        order_by(func.count(Measurement.station).desc()).\
-        group_by(Measurement.station).first()
-    most_active_station = active_station[0] 
+    # Retrieve the temperature observations for the most active station within the last year
+    tobs_results = session.query(Measurement.date, Measurement.tobs).\
+        filter(Measurement.station == most_active_station[0]).\
+        filter(Measurement.date >= one_year_ago).all()
 
-    session.close() 
-     # active_station returns: ('USC00519281', 2772), index to get the first position to isolate most active station number
-    print(most_active_station)
-    # returns: USC00519281  
+    session.close()
 
-    # Create a query to find dates and tobs for the most active station (USC00519281) within the last year (> 2016-08-23)
+    # Create a list of dictionaries to store the date and temperature observations
+    tobs_list = []
+    for date, tobs in tobs_results:
+        tobs_dict = {"date": date, "tobs": tobs}
+        tobs_list.append(tobs_dict)
 
-    dates_tobs_last_year_query_results = session.query(Measurement.date, Measurement.tobs, Measurement.station).\
-        filter(Measurement.date > query_start_date).\
-        filter(Measurement.station == most_active_station) 
-    
+    # Return the JSON representation of the temperature observations
+    return jsonify(tobs_list)
 
-    # Create a list of dates,tobs,and stations that will be appended with dictionary values for date, tobs, and station number queried above
-    dates_tobs_last_year_query_values = []
-    for date, tobs, station in dates_tobs_last_year_query_results:
-        dates_tobs_dict = {}
-        dates_tobs_dict["date"] = date
-        dates_tobs_dict["tobs"] = tobs
-        dates_tobs_dict["station"] = station
-        dates_tobs_last_year_query_values.append(dates_tobs_dict)
-        
-    return jsonify(dates_tobs_last_year_query_values) 
-
+#################################################
 # Create a route that when given the start date only, returns the minimum, average, and maximum temperature observed for all dates greater than or equal to the start date entered by a user
 
 @app.route("/api/v1.0/<start>")
