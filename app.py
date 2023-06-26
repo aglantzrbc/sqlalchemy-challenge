8cac0df099bf78b978ab04d9b6bc1523d8a6bdbf
aglantzrbc <adamglantz@yahoo.com>
2023-06-25 22:14:36 -0400

    initial commit

--- app.py
+++ app.py
@@ -39,30 +39,31 @@ app = Flask(__name__)
 # Flask Routes
 #################################################
 
+
 #################################################
-# TASK: Create welcome route, then list all available api routes
+''' TASK /: Create welcome route, then list all available api routes '''
 # Create a welcome route
 @app.route("/")
 def welcome():
     # List all available api routes as hyperlinks, annotated for clarity
     return (
-        "<h2>Available API Routes:</h2>"
+        "<h2>Available API Routes for JSON Data:</h2>"
         "<ul>"
-        "<li><a href=\"/api/v1.0/precipitation\">/api/v1.0/precipitation</a></li>"
-        "<li><a href=\"/api/v1.0/stations\">/api/v1.0/stations</a></li>"
-        "<li><a href=\"/api/v1.0/tobs\">/api/v1.0/tobs</a></li>"
-        "<li><a href=\"/api/v1.0/start\">/api/v1.0/start</a></li>"
+        "<li>Last 12 months of precipitation data -- <a href=\"/api/v1.0/precipitation\">/api/v1.0/precipitation</a></li>"
+        "<li>Details of temperature observation stations -- <a href=\"/api/v1.0/stations\">/api/v1.0/stations</a></li>"
+        "<li>Last 12 months of temperature data from the most active temperature observation station -- <a href=\"/api/v1.0/tobs\">/api/v1.0/tobs</a></li>"
         "</ul>"
         "<ul>"
-        "<li>/api/v1.0/<strong>[start]</strong>/<strong>[end]</strong></li>"
+        "<li>Temperature benchmark data from variable date ranges -- /api/v1.0/<strong>[start]</strong>/<strong>[end]</strong></li>"
         "</ul>"
-        "<p><small><strong>Note:</strong> for the last route, replace <strong>[start]</strong> and <strong>[end]</strong> with start and end dates using the format: <strong>YYYY-mm-dd/YYYY-mm-dd</strong>.<br/>Copy and paste the result into your browser, preceded by '<strong>http://localhost:5000/</strong>'.<br/><em>Example:</em> <a href=\"/api/v1.0/2016-08-23/2017-08-23\">http://localhost:5000/api/v1.0/2016-08-23/2017-08-23</a>.</small></p>"
+        "<p><small><strong>Note:</strong> for the last route, replace <strong>[start]</strong> and <strong>[end]</strong> with start and end dates using the format: <strong>YYYY-mm-dd/YYYY-mm-dd</strong>.<br/>Copy and paste the result into your browser, preceded by '<strong>http://localhost:5000/</strong>'.<br/><em>Example:</em> <a href=\"/api/v1.0/2016-08-23/2017-08-23\">http://localhost:5000/api/v1.0/2016-08-23/2017-08-23</a>.<br>Date ranges outside the scope of actual dates occurring in the dataset will be defaulted to the earliest and latest dates in the data.</small></p>"
 
-        "<p><small><strong>Note:</strong> All the links assume the user employs <strong>port 5000</strong> for Flask output.<br/>If not, the links won't work, and you'll have to paste each route into your browser preceded by '<strong>http://localhost:XXXX/</strong>', where '<strong>XXXX</strong>' is your port of choice.<br/><em>Example:</em> <a href=\"/api/v1.0/2016-08-23/2017-08-23\">http://localhost:XXXX/api/v1.0/start</a>.</small></p>"
+        "<p><small><strong>Note:</strong> All the links assume the user employs <strong>port 5000</strong> for Flask output.<br/>If not, the links won't work, and you'll have to paste each route into your browser preceded by '<strong>http://localhost:XXXX/</strong>', where '<strong>XXXX</strong>' is your port of choice.<br/><em>Example:</em> <a href=\"/api/v1.0/precipitation\">http://localhost:XXXX/api/v1.0/precipitation</a>.</small></p>"
     )
 
+
 #################################################
-'''TASK: Convert the query results from the precipitation analysis (i.e. retrieve only the last 12 months of data) to a dictionary using [date] as the key and [prcp] as the value. Return the JSON representation of the dictionary.'''
+''' TASK PRECIPITATION: Convert the query results from the precipitation analysis (i.e. retrieve only the last 12 months of data) to a dictionary using [date] as the key and [prcp] as the value. Return the JSON representation of the dictionary. '''
 # Create a precipitation route
 @app.route("/api/v1.0/precipitation")
 def precipitation():
@@ -92,11 +93,12 @@ def precipitation():
         precipitation_dict["date"] = date
         precipitaton_query_values.append(precipitation_dict)
 
-    # Return a JSON list of precipitation_query_values
+    # Return a JSON list of precipitation query values
     return jsonify(precipitaton_query_values)
 
+
 #################################################
-# TASK: Return a JSON list of stations from the dataset
+''' TASK STATIONS: Return a JSON list of stations from the dataset '''
 # Create a station route
 @app.route("/api/v1.0/stations")
 def station(): 
@@ -118,14 +120,16 @@ def station():
         stations_values_dict['id'] = id
         stations_values.append(stations_values_dict)
 
-    # Return a JSON list of stations_values
+    # Return a JSON list of stations values
     return jsonify (stations_values) 
 
+
 #################################################
-'''TASK: Query the dates and temperature observations of the most-active station for the previous year of data. Return a JSON list of temperature observations for the previous year.'''
+''' TASK TOBS: Query the dates and temperature observations of the most-active station for the previous year of data. Return a JSON list of temperature observations for the previous year. '''
 # Create a tobs route
 @app.route("/api/v1.0/tobs")
 def tobs():
+
     # Establish session
     session = Session(engine)
 
@@ -157,62 +161,53 @@ def tobs():
     # Return the JSON representation of the temperature observations
     return jsonify(tobs_list)
 
+
 #################################################
-# Create a route that when given the start date only, returns the minimum, average, and maximum temperature observed for all dates greater than or equal to the start date entered by a user
-
-@app.route("/api/v1.0/<start>")
-# Define function, set "start" date entered by user as parameter for start_date decorator 
-def start_date(start):
-    session = Session(engine) 
-
-    """Return a JSON list of the minimum temperature, the average temperature, and the max temperature for a given start date."""
-
-    # Create query for minimum, average, and max tobs where query date is greater than or equal to the date the user submits in URL
-    start_date_tobs_results = session.query(func.min(Measurement.tobs),func.avg(Measurement.tobs),func.max(Measurement.tobs)).\
-        filter(Measurement.date >= start).all()
-    
-    session.close() 
-
-    # Create a list of min,max,and average temps that will be appended with dictionary values for min, max, and avg tobs queried above
-    start_date_tobs_values =[]
-    for min, avg, max in start_date_tobs_results:
-        start_date_tobs_dict = {}
-        start_date_tobs_dict["min"] = min
-        start_date_tobs_dict["average"] = avg
-        start_date_tobs_dict["max"] = max
-        start_date_tobs_values.append(start_date_tobs_dict)
-    
-    return jsonify(start_date_tobs_values)
-
-# Create a route that when given the start date only, returns the minimum, average, and maximum temperature observed for all dates greater than or equal to the start date entered by a user
-
+''' TASK START-END: Return a JSON list of the minimum temperature, the average temperature, and the maximum temperature for a specified start or start-end range: '''
+''' For a specified start, calculate TMIN, TAVG, and TMAX for all the dates greater than or equal to the start date. '''
+''' For a specified start date and end date, calculate TMIN, TAVG, and TMAX for the dates from the start date to the end date, inclusive. '''
 @app.route("/api/v1.0/<start>/<end>")
-
-# Define function, set start and end dates entered by user as parameters for start_end_date decorator
 def Start_end_date(start, end):
+
+    # Establish session
     session = Session(engine)
 
-    """Return a list of min, avg and max tobs between start and end dates entered"""
-    
-    # Create query for minimum, average, and max tobs where query date is greater than or equal to the start date and less than or equal to end date user submits in URL
+    """ Return a list of min, avg, and max tobs between start and end dates entered """
 
+    # Get the earliest and latest dates in the dataset
+    earliest_date = session.query(func.min(Measurement.date)).scalar()
+    latest_date = session.query(func.max(Measurement.date)).scalar()
+
+    # Set default values for start and end dates if they are outside the dataset date range
+    if start < earliest_date:
+        start = earliest_date
+
+    if end > latest_date:
+        end = latest_date
+
+    # Query for minimum, average, and maximum tobs within the specified date range
     start_end_date_tobs_results = session.query(func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)).\
         filter(Measurement.date >= start).\
         filter(Measurement.date <= end).all()
 
+    # Close session
     session.close()
-  
-    # Create a list of min,max,and average temps that will be appended with dictionary values for min, max, and avg tobs queried above
-    start_end_tobs_date_values = []
-    for min, avg, max in start_end_date_tobs_results:
-        start_end_tobs_date_dict = {}
-        start_end_tobs_date_dict["min_temp"] = min
-        start_end_tobs_date_dict["avg_temp"] = avg
-        start_end_tobs_date_dict["max_temp"] = max
-        start_end_tobs_date_values.append(start_end_tobs_date_dict) 
-    
 
+    # Create a list of dictionaries to store the temperature data
+    start_end_tobs_date_values = []
+    for min_temp, avg_temp, max_temp in start_end_date_tobs_results:
+        tobs_dict = {
+            "min_temp": min_temp,
+            "avg_temp": avg_temp,
+            "max_temp": max_temp
+        }
+        start_end_tobs_date_values.append(tobs_dict)
+
+    # Return the JSON representation of the temperature observation data
     return jsonify(start_end_tobs_date_values)
-   
+
+
+#################################################
+# Make sure the Flask application is only run if the script is being executed directly
 if __name__ == '__main__':
     app.run(debug=True) 
